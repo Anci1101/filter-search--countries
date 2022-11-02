d920055a2b54f395d48419a2c4fe4e7c8bc35fea
Ana <anci11011@gmail.com>
2022-11-02 20:10:16 +0100

    fetch and display countries data

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-const Country = () => {
+const Country = ({countryname, population,region, capital, flag }) => {
+
   return (
     <>
     <div className='country'>
-      <div className='country-flag'></div>
+      <div className='country-flag'>
+        <img src={flag} alt={flag} /></div>
       <div className='country-details'>
-        <h4>Germany</h4>
-        <p>Population: <span>81.770.900</span></p>
-        <p>Region: <span>Europe</span></p>
-        <p>Capital: <span>Berlin</span></p>
+        <h4>{countryname}</h4>
+        <p>Population: <span>{population}</span></p>
+        <p>Region: <span>{region}</span></p>
+        <p>Capital: <span>{capital}</span></p>
       </div>
     </div>
     </>
--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import useFetch from '../hooks/useFetch';
 import Country from "./Country";
 
+
 const CountryList = () => {
+  const {data:countries, loading, error} = useFetch('https://restcountries.com/v3.1/all')
+  console.log(countries, 'data');
+
   return (
-    <div>
-      <Country/>
+    <div className='country-wrapper grid'>
+      {countries.map((country)=>(
+        <Country 
+          countryname={country.name.common}
+          population={country.population}
+          region={country.region}
+          capital={country.capital}
+          flag={country.flags.png}/>
+      ))}
+      
     </div>
   )
 }
