0bcb8399cc9ea183ba39608b3e36d6a8b29f134c
Ana <anci11011@gmail.com>
2022-12-06 15:39:01 +0100

    add search by name and enable to search by region and by search name together

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
-const Search = () => {
-
-  const [inputValue, setInputValue] = useState('')
-  const handleSearch = () => {
-    console.log( inputValue, 'input value');
-  }
+const Search = ({handleSearch}) => {
 
   return (
     <div className='search-bar'>
       <button onClick={handleSearch}><FontAwesomeIcon icon={faSearch} /></button>
-      <input type="text" onChange={(e)=>setInputValue(e.target.value)} placeholder='Search for a country...'/>
+      <input type="text" onChange={handleSearch} placeholder='Search for a country...'/>
     </div>
   )
 }
